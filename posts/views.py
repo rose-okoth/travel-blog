fea5457ae28ae052399639680430e1a0d9939ce1
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 15:33:30 +0300

    sdded post_detail html

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
@@ -7,7 +7,13 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, id=1)
+    context = {
+            "title":instance.title,
+            "instance":instance
+        }
+
+    return render(request,"post_detail.html",context)
 
 def post_list(request):
     queryset = Post.objects.all()
