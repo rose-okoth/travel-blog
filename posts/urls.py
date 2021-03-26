68babf447a16e1294e2d6489f69881e31bfb5247
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 13:38:47 +0300

    updated views.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
 from django.conf.urls import url
+from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^$','posts.views.post_home'),
+    url(r'^$','posts.views.post_list'),
+    url(r'^create/$','posts.views.post_create'),
+    url(r'^detail/$','posts.views.post_detail'),
+    url(r'^update/$','posts.views.post_update'),
+    url(r'^delete/$','posts.views.post_delete'),
 ]
