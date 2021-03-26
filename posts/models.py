d9264750ae963e22263db726cea54c29cc1fd0a0
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 12:26:23 +0300

    add post model to admin site

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=150)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
