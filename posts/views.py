b5cb3886ab3ab588e821d75c57b117d99f44377a
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 18:12:16 +0300

    added the delete function

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from .models import Post
@@ -53,5 +53,8 @@ def post_update(request, id=None):
         }
     return render(request,"post_form.html",context) 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully Deleted!")
+    return redirect("posts:list")
