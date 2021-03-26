3c670e1ae2840b76fa23aa589f6bb04f7eaf07fc
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 18:42:12 +0300

    added in the base html file

--- posts/views.py
+++ posts/views.py
@@ -36,7 +36,7 @@ def post_list(request):
             "object_list":queryset,
             "title":"List"
         }
-    return render(request,"index.html",context)
+    return render(request,"base.html",context)
     
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
