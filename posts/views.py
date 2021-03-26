8b81bb81e21ad4bafe14aeb2ec3d794fae44c142
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 15:20:41 +0300

    added in context and query sets

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def post_create(request):
@@ -9,7 +10,21 @@ def post_detail(request):
     return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
-    return render(request,"index.html",{})
+    queryset = Post.objects.all()
+    context = {
+            "object_list":queryset,
+            "title":"List"
+        }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title":"My List"
+    #     }
+    # else:
+    #     context = {
+    #         "title":"List"
+    #     }
+    return render(request,"index.html",context)
     
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
