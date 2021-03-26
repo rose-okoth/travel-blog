bdbc98c46f7b4d21716fc52f84f26bba97281ef1
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 16:29:17 +0300

    added dynamic urls to the post titles

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +10,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"id": self.id})
