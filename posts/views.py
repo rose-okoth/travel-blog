432a149750ec9f862b41cfaa6994f5c6c73f202e
rose-okoth <okoth.rose0@gmail.com>
2021-03-26 16:39:46 +0300

    added in the create form and html file

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm()
+    context = {
+        "form":form,
+    }
+    return render(request,"post_form.html",context)
 
 def post_detail(request,id):
     instance = get_object_or_404(Post, id=id)
